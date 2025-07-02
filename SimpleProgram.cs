e734176addfd48653956e9dd605673926d45de2b
sujangali <sujan.fullstack@gmail.com>
2025-07-02 16:49:28 +0530

    Simple Program

---
+++ SimpleProgram.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class SimpleProgram
+{
+    static void Main(string[] args)
+    {
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("This is My C# Program");
+        }
+    }
+}
+
